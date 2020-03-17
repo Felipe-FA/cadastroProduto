42424f6002118c5e82504320df1f8f259100f597
Felipe Ferreira Assis <fferreia@everis.com>
2020-03-17 14:25:54 -0300

    Metodos rest adicionados

--- src/main/java/com/felipe/cadastro/services/CategoriaService.java
+++ src/main/java/com/felipe/cadastro/services/CategoriaService.java
@@ -20,4 +20,18 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+		
+	}
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	public void delete(Integer id) {
+		find(id);
+		repo.deleteById(id);
+	}
 }
